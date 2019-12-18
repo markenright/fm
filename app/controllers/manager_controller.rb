5f711dcd69f0caca5eed0b87fe986b75e5d2bdf9
markenright <enright.mark@gmail.com>
2019-12-18 10:54:27 -0500

    fixed controllers

--- app/controllers/manager_controller.rb
+++ app/controllers/manager_controller.rb
@@ -1,34 +1,73 @@
 class ManagerController < ApplicationController
 #index page - list of managers - pick or create new
 
-    #get all managers
+    
+
+    get '/' do
+        redirect '/managers'
+    end
+
+#1 get all managers
     get '/managers' do
         @managers = Manager.all
         erb :'managers/index'
     end
 
-    #get by id, dynamic
-    get "/managers/:id" do
+    #2 sends to the new manager page
+    get '/managers/new' do
+        erb :'managers/new'
+    end
+
+    #3 get by id, dynamic
+    get '/managers/:id' do
         @manager = Manager.find(params[:id])
-        erb :"managers/show"
+        if @manager
+             erb :'managers/show'
+        else
+            redirect '/managers'
+        end
+
+       
     end
 
-    #sends to the new manager page
-    get "/managers/new" do
-        erb :"managers/new"
+    
+
+    #4 adds new manager to database
+    post '/managers' do
+        manager = Manager.create(name: params[:name], age: params[:age])
+        
+        if manager.save
+            redirect "/managers"
+        else
+            redirect "managers/new"
+        end
+
     end
 
-    #adds new manager to database
-    post "/managers" do
-        manager = Manager.create(params[:manager])
-        redirect '/managers/#{manager.id}'  
+   
+
+    #5 go to edit manager page
+    get '/managers/:id/edit' do
+        puts "Get here into edit page?"
+        @manager = Manager.find(params[:id])
+        puts @manager.name
+        erb :'managers/edit'  
     end
 
-    #edit manager
-    get "/managers/:id/edit" do
-        @manager = manager.find(params[:id])
-        erb :"managers/edit"  
+    #6 edit by id and save
+    patch '/managers/:id' do
+        puts "can we get here?????????????????"
+
+        manager= Manager.find(params[:id])
+        manager.update(params[:manager])
+        redirect "/managers/#{manager.id}"  
     end
+
+    delete "/managers/:id" do
+    manager = Manager.find(params[:id])
+    manager.destroy
+    redirect "/managers"
+end
 
 
 end
